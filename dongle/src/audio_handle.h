7c87be50bf42799db58d3455e7d9d77e2b666fcc
Noy <noy.luo@nordicsemi.no>
2025-09-10 14:41:00 +0800

    add dongle code of 54l15 version to this project

---
+++ dongle/src/audio_handle.h
@@ -0,0 +1,49 @@
+
+#ifndef __USB_AUDIO_APP_H__
+#define __USB_AUDIO_APP_H__
+
+#include <stdint.h>
+#include "opus_application_config.h"
+#include "opus.h"
+
+
+
+#define MAX_SAMPLE_RATE             		16000
+#define SAMPLE_BIT_WIDTH            		16
+#define BYTES_PER_SAMPLE            		sizeof(int16_t)
+
+
+#define USB_FRAME_SIZE_STEREO              (((MAX_SAMPLE_RATE * BYTES_PER_SAMPLE) / 1000) *2)
+
+#define CONFIG_FIFO_FRAME_SPLIT_NUM         100
+
+
+/* Size of a block for 1 ms of audio data. */
+#define BLOCK_SIZE(_sample_rate, _number_of_ms) \
+	(BYTES_PER_SAMPLE * (_sample_rate / 1000) * _number_of_ms)
+
+
+#define PCM_BLOCK_SIZE				BLOCK_SIZE(MAX_SAMPLE_RATE, 2) 			// 1 ms
+#define FRAME_SIZE                  ((MAX_SAMPLE_RATE / 1000) * 2)
+#define PCM_FRAME_BYTES 			(CONFIG_AUDIO_FRAME_SIZE_SAMPLES * CONFIG_OPUS_CHANNELS)
+
+#define PCM_BLOCK_COUNT             100
+
+#define MAX_PAYLOAD_SIZE	(80 + 4) // 80 bytes for Opus payload + 4 bytes for packet ID
+
+
+/** @brief Enhanced ShockBurst payload.
+ *
+ *  The payload is used both for transmissions and for acknowledging a
+ *  received packet with a payload.
+ */
+struct audio_payload {
+    uint8_t dev_id; /**< Device ID, used to identify the device that sent the packet. */
+	uint8_t length; /**< Length of the packet when not in DPL mode. */
+	uint8_t data[MAX_PAYLOAD_SIZE]; /**< The payload data and devID. */
+};
+
+
+
+
+#endif
